d224f0e0096061932bc6e4cf7fb3ad493dd9515c
Alexander Raschke <alexander.raschke@uni-ulm.de>
2025-12-19 00:50:13 +0100

    Change connection name from 'remote' to 'vps'
    Updated podman-entrypoint.sh to add connection with a new name.

--- podman-entrypoint.sh
+++ podman-entrypoint.sh
@@ -43,7 +43,8 @@ chmod 600 $CONTAINER_SSHKEY
 eval $(ssh-agent)
 ssh-add $CONTAINER_SSHKEY
 
-podman system connection add --identity "$CONTAINER_SSHKEY" remote "$CONTAINER_HOST"
+echo "Add ${CONTAINER_HOST} to connections ... "
+podman system connection add --identity "$CONTAINER_SSHKEY" vps "$CONTAINER_HOST"
 
 if  [ -n "$INPUT_DOCKER_LOGIN_PASSWORD" ] || [ -n "$INPUT_DOCKER_LOGIN_USER" ] || [ -n "$INPUT_DOCKER_LOGIN_REGISTRY" ]; then
   echo "Command: podman login -u ${INPUT_DOCKER_LOGIN_USER} -p $INPUT_DOCKER_LOGIN_PASSWORD $INPUT_DOCKER_LOGIN_REGISTRY"
